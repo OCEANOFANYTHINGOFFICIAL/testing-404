2444d565d08e3d0bac016fb26a8ef2c8d073e36a
oceanofanything <oceanofanything@gmail.com>
2021-06-10 18:48:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// Attribute Javascript For This Template Start
+console.log("Hayato Template")
+console.log("Created By Nakshatra Ranjan Saha [ Ocean Of Anything ].")
+console.log("Visit Official Site: https://oceanofanythingofficial.github.io")
+console.log("This Is A Paid Template. So If You Want To Edit Or Use This Template, Then Please Buy It.")
+console.log("Template Cost 5$")
+// Attribute Javascript For This Template close
